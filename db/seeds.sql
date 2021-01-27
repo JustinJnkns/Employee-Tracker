4e243396fe2d64d8e163e262f6f25f53bad92d9a
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2021-01-27 01:36:21 -0600

    Add files via upload
    Connected App to express and got server running. Also created tables in database with sqlite

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (department_name)
+VALUES
+('Development'),
+('Marketing'),
+('Mechanic'),
+('Plumber')
+
+INSERT INTO roles (title,salary,department_id)
+VALUES
+(expert,200000,1),
+(novice,100000,3),
+(inexperienced,50000,2)
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES
+('Wolf','Wallstreet',1,1),
+('Tanner','Real',2,3),
+('Real','man',2,1),
+('Def','Bored',1,1)
+
+
+
